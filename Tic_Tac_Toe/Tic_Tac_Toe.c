98a7591bfd8c629e409c67df5845ea2abcec9d77
WiktorZmiendak <wiktor.zmiendak@gmail.com>
2023-11-19 15:36:27 +0100

    Update Tic_Tac_Toe

--- Tic_Tac_Toe/Tic_Tac_Toe.c
+++ Tic_Tac_Toe/Tic_Tac_Toe.c
@@ -2,20 +2,81 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <time.h>
-
+#include <windows.h>
 
 char map[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 
-int check_draw() {
-    for(int i = 0; i < 9; i++) {
-        if(map[i] == ' ') {
-            return 0;
-        }
-    }
-    return 1;
+//Functions declaration
+int check_draw();
+int check_if_won(char ch);
+void select_game_mode();
+void reset_map();
+void print_board();
+void print_logo();
+void single_mode();
+void multi_mode();
+
+int main() {
+    srand(time(NULL));
+    
+    select_game_mode();
+
+    getch();
+
+    return 0;
 }
 
-void map_print() {
+void select_game_mode() {
+    char ans_mode;
+    char ans_next;
+    while(1) {
+        print_logo();
+        printf("Select game mode\nSingleplayer [s]\nMultiplayer [m]\n");
+        scanf(" %c", &ans_mode);
+
+        if(ans_mode == 's') {
+            single_mode();
+        }
+        else if(ans_mode == 'm') {
+            multi_mode();
+        }
+        else {
+            printf("Wrong input\n");
+            Sleep(1000);
+            system("cls");
+     
+        }
+        printf("\nReady for the next game? [Y/N]\n");
+        if(scanf(" %c", &ans_next) == 1) {
+            if (ans_next == 'Y' || ans_next == 'y') {
+                system("cls");
+                reset_map();
+                continue;
+            }
+            else {
+                printf("\nSee you later!\n");
+                break;
+            }
+        }
+    }
+}
+
+void reset_map() {
+    for (int i = 0; i < 9; i++) {
+        map[i] = ' ';
+    }
+}
+
+void print_logo() {
+    printf("___________ __         ___________               ___________            \n");
+    printf("\\__    ___/|__| ____   \\__    ___/____    ____   \\__    ___/___   ____  \n");
+        printf("  |    |   |  |/ ___\\    |    |  \\__  \\ _/ ___\\    |    | /  _ \\_/ __ \\ \n");
+    printf("  |    |   |  \\  \\___    |    |   / __ \\\\  \\___    |    |(  <_> )  ___/ \n");
+    printf("  |____|   |__|\\___  >   |____|  (____  /\\___  >   |____| \\____/ \\___  >\n");
+    printf("                   \\/                 \\/     \\/                      \\/ \n\n\n");
+}
+
+void print_board() {
     system("cls");
     printf("   |   |   \n");
     printf(" %c | %c | %c \n", map[0], map[1], map[2]);
@@ -40,29 +101,42 @@ int check_if_won(char ch) {
     else return 0;
 }
 
+int check_draw() {
+    for(int i = 0; i < 9; i++) {
+        if(map[i] == ' ') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 void single_mode() {
     while(1) {
         int position;
 
         while(1) {
-            map_print();
+            print_board();
             printf("[0][1][2]\n[3][4][5]\n[6][7][8]\nWhere do you want to enter X?\n");
-            scanf("%d", &position);
-            if(map[position] == 'O') {
-                continue;
+            if(scanf("%d", &position) == 1) {
+                if(map[position] != ' ' || position < 0 || position > 8) {
+                    continue;
+                }
+                map[position] = 'X';
+                break;
             }
-            map[position] = 'X';
-            break;
+            else {
+                while(getchar() != '\n');
+            }
         }
 
         if(check_if_won('X') == 1) {
-                map_print();
-                printf("Congrats, You won!\n");
-                break;
-            }
+            print_board();
+            printf("Congrats, You won!\n");
+            break;
+        }
         
         if(check_draw() == 1) {
-            map_print();
+            print_board();
             printf("Draw!\n");
             break;
         }
@@ -76,13 +150,13 @@ void single_mode() {
         }
 
         if(check_if_won('O') == 1) {
-                map_print();
+                print_board();
                 printf("You lost!\n");
                 break;
             }
         
         if(check_draw() == 1) {
-            map_print();
+            print_board();
             printf("Draw!\n");
             break;
         }
@@ -95,70 +169,59 @@ void multi_mode() {
         int position;
         
         while(1) {
-            map_print();
+            print_board();
             printf("[0][1][2]\n[3][4][5]\n[6][7][8]\nWhere do you want to enter X?\n");
-            scanf("%d", &position);
-            if(map[position] == 'O') {
-                continue;
+            if(scanf("%d", &position) == 1) {
+                if(map[position] != ' ' || position < 0 || position > 8) {
+                    continue;
+                }
+                map[position] = 'X';
+                break;
             }
-            map[position] = 'X';
-            break;
+            else {
+                while(getchar() != '\n');
+            }
         }
 
         if(check_if_won('X') == 1) {
-                map_print();
-                printf("Congrats Player 1, You won!\n");
-                break;
-            }
+            print_board();
+            printf("Congrats Player 1, You won!\n");
+            break;
+        }
         
         if(check_draw() == 1) {
-            map_print();
+            print_board();
             printf("Draw!\n");
             break;
         }
 
         while(1) {
-            map_print();
+            print_board();
             printf("[0][1][2]\n[3][4][5]\n[6][7][8]\nWhere do you want to enter O?\n");
-            scanf("%d", &position);
-            if(map[position] == 'X') {
-                continue;
+            if(scanf("%d", &position) == 1) {
+                if(map[position] != ' ' || position < 0 || position > 8) {
+                    continue;
+                }
+                map[position] = 'O';
+                break;
             }
-            map[position] = 'O';
-            break;
+            else {
+                while(getchar() != '\n');
+            }
         }
         
         if(check_if_won('O') == 1) {
-                map_print();
-                printf("Congrats Player 2, You won!\n");
-                break;
-            }
+            print_board();
+            printf("Congrats Player 2, You won!\n");
+            break;
+        }
 
         if(check_draw() == 1) {
-            map_print();
+            print_board();
             printf("Draw!\n");
             break;
         }
 
     }
 
-}
-
-int main() {
-    srand(time(NULL));
-    char ans_mode;
-    printf("Select game mode\nSingleplayer [s]\nMultiplayer [m]\n");
-    scanf(" %c", &ans_mode);
-    if(ans_mode == 's') {
-        single_mode();
-    }
-    else if(ans_mode == 'm') {
-        multi_mode();
-    }
-    else {
-        printf("Wrong input\n");
-    }
-
-    getch();
-    return 0;
 }
